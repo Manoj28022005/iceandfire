1ec881db50cc80aacf4821aa8034d1d6d5762cdc
Manoj28022005 <132426115+Manoj28022005@users.noreply.github.com>
2024-11-12 19:17:49 +0530

    Update added only vercel web link removed local host

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 mongoDB();
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://iceandfire-frontend.vercel.app"], // Allow specific origins
+    origin: ["https://iceandfire-frontend.vercel.app"], // Allow specific origins
     methods: ["POST", "GET"], // Allow specific methods
     credentials: true // Allow cookies with requests
 }));
